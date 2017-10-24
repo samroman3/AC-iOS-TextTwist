1ab9d25c81c3020c1c35aca4cd8eda04a3b5c09f
Ben Stone <benjamin.h.stone@gmail.com>
2017-10-24 13:51:49 -0400

    moved content from master into solution

--- TextTwist/TextTwistModel.swift
+++ TextTwist/TextTwistModel.swift
@@ -9,30 +9,4 @@
 import Foundation
 
 class TextTwistModel {
-    func guess(_ word: String) -> Bool {
-        return currentInfo.words.contains(word)
-    }
-    func addLetter(_ c: Character) {
-        self.currentLetters.append(c)
-    }
-    
-    func removeLetter(_ c: Character) -> Bool {
-        guard let removalIndex = currentLetters.index(of: c) else {
-            return false
-        }
-        self.currentLetters.remove(at: removalIndex)
-        return true
-    }
-    var currentLetters: String
-    var guessedWords: [String] = []
-    func resetCurrentLetters() {
-        self.currentLetters = currentInfo.letters
-    }
-    private var currentInfo: TestTwistInfo
-    
-    init() {
-        let randIndex = Int(arc4random_uniform(3))
-        self.currentInfo = WordData.allInfo[randIndex]
-        self.currentLetters = WordData.allInfo[randIndex].letters
-    }
 }
